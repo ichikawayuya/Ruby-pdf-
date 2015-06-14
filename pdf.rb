593f63af035e123c9e2983e20d17ca8e31d4b1c7
ichikawayuya <ichikun.cocoa+github@gmail.com>
2015-06-14 20:16:40 +0900

    Update pdf.rb

--- pdf.rb
+++ pdf.rb
@@ -4,46 +4,63 @@ require 'pdf/reader'
 
 
 
-#ページ毎に文字列を格納する
-str = Array.new()
-count = 0
-page = 0
-#ファイルの指定
-File.open("/Users/user/Desktop/test.pdf", "rb") do |io|
-  # インスタンス化
-  reader = PDF::Reader.new(io)
+#ページ枚数を取得する
+def Page()
+	File.open("/Users/ichikawayuya/Desktop/guraduate.pdf", "rb") do |io|
+	  reader = PDF::Reader.new(io)
+	  return reader.page_count
+	end
+end
+#pdfの文字を格納する
+def pdf_to_array(page)
 	#ページの枚数だけ配列を用意
-	str = Array.new(reader.page_count)
-	page = reader.page_count
-  # ページ単位で文字列を格納
-  reader.pages.each do |page|
-	str[count] = page.text
-	count = count + 1
-  end
+	str = Array.new(page)
+	count = 0
+
+	File.open("/Users/ichikawayuya/Desktop/guraduate.pdf", "rb") do |io|
+	  # インスタンス化
+	  reader = PDF::Reader.new(io)	
+	  # ページ単位で文字列を格納
+	  reader.pages.each do |page|
+		str[count] = page.text
+		count = count + 1
+	  end
+	end
+	return str
+end
+
+
+#正規表現を用いて必要な文字列を取得する
+def RegularExpression(str, page)
+	text = Array.new(1000)
+	count = 0
+	
+	for i in 0..page do
+		loop{
+			if/([^\s]*)\b[0-9]*/ =~ str[i] then
+  				text[count] = $&
+				#正規表現の結果から基の文字列を削除する 
+				str[i].slice!("#{$&}")
+				 count= count + 1
+			else
+				break
+			end
+		}
+	end
+	
+	return text
 end
 
 
 
-text = Array.new(1000)
-count = 0
-
-#正規表現(Regular Explesion)を用いて空白を取り除き，必要な部分だけ取り除く
-for i in 0..page do
-	loop{
-		if/([^\s]*)\b[0-9]*/ =~ str[i] then
-  			text[count] = $&
-			 str[i].slice!("#{$&}")
-			 count= count + 1
-		else
-			break
-		end
-	}
+if __FILE__ == $0
+	page = Page()
+	array = pdf_to_array(page)
+	str =RegularExpression(array, page)
+	for i in 0 .. 1000 do
+		print i 
+		print ":"
+		puts str[i] 
+	end
+	
 end
-
-#格納した文字列を出力してみる
-for i in 0 .. 1000 do
-	print i 
-	print ":"
-	puts text[i] 
-end
-
